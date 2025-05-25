cc837b375c7ea9b7cd48c40cf56a52ac59952806
Amani <amanikhalidd@gmail.com>
2025-05-25 15:46:16 +0400

    Created Trainees Table

---
+++ SubqueryUnionTask.sql
@@ -0,0 +1,16 @@
+--create database
+
+Create database SubqueryUnionTask;
+
+use SubqueryUnionTask
+
+-- Tables
+-- Trainees Table
+CREATE TABLE Trainees (
+ TraineeID INT PRIMARY KEY,
+ FullName VARCHAR(100),
+ Email VARCHAR(100),
+ Program VARCHAR(50),
+ GraduationDate DATE
+);
+
